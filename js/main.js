21c1443638e51532b71dacad62be2e07e1bce9cb
Audrey_Kow <aud.kow88@gmail.com>
2018-08-28 16:15:29 +0800

    created function for game

--- js/main.js
+++ js/main.js
@@ -3,17 +3,7 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree); */
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
+console.log("User flipped " + cardThree); 
 
 if (cardsInPlay.length === 2){
 	console.log ("Two cards played.");
@@ -22,7 +12,30 @@ if (cardsInPlay.length === 2){
 	} else {
 		alert("Awww.. Try again.");
 	}
-} else {
-	console.log("Error");
+	*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+	if (cardsInPlay.length < 2){
+		return ;
+	}else if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 }
+}
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+} 
+
+flipCard(0);
+flipCard(3);
+
+
+
 
